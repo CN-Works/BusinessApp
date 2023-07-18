ae92af9c551d0600cf61b847e3d8572e423f4711
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-18 14:28:51 +0200

    Chore : listing employees

--- src/Controller/EmployeeController.php
+++ src/Controller/EmployeeController.php
@@ -2,17 +2,22 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Employee;
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmployeeController extends AbstractController
 {
     #[Route('/employee', name: 'app_employee')]
-    public function index(): Response
+    public function index(EmployeeRepository $employeeRepository): Response
     {
+        // getting all employees from SocietyRepository
+        $employees = $employeeRepository->findAll();
         return $this->render('employee/index.html.twig', [
-            'controller_name' => 'EmployeeController',
+            "employees" => $employees,
         ]);
     }
 }
