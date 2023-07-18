cfb7aaec7bde973ba56939143b1ff5ff03f70a27
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-18 09:45:48 +0200

    Chore : migration

---
+++ migrations/Version20230718073237.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230718073237 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE employee (id INT AUTO_INCREMENT NOT NULL, society_id INT NOT NULL, firstname VARCHAR(100) NOT NULL, lastname VARCHAR(255) NOT NULL, dateofbirth DATETIME NOT NULL, datehired DATETIME NOT NULL, INDEX IDX_5D9F75A1E6389D24 (society_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE society (id INT AUTO_INCREMENT NOT NULL, label VARCHAR(255) NOT NULL, creationdate DATETIME NOT NULL, adress VARCHAR(255) DEFAULT NULL, postalcode VARCHAR(255) DEFAULT NULL, city VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE messenger_messages (id BIGINT AUTO_INCREMENT NOT NULL, body LONGTEXT NOT NULL, headers LONGTEXT NOT NULL, queue_name VARCHAR(190) NOT NULL, created_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', available_at DATETIME NOT NULL COMMENT \'(DC2Type:datetime_immutable)\', delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\', INDEX IDX_75EA56E0FB7336F0 (queue_name), INDEX IDX_75EA56E0E3BD61CE (available_at), INDEX IDX_75EA56E016BA31DB (delivered_at), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE employee ADD CONSTRAINT FK_5D9F75A1E6389D24 FOREIGN KEY (society_id) REFERENCES society (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE employee DROP FOREIGN KEY FK_5D9F75A1E6389D24');
+        $this->addSql('DROP TABLE employee');
+        $this->addSql('DROP TABLE society');
+        $this->addSql('DROP TABLE messenger_messages');
+    }
+}
