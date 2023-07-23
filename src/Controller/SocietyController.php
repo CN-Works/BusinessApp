574af95e7a45d0bb873382833fd8695a35b15e64
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-24 01:38:51 +0200

    Feat : Create Society form

--- src/Controller/SocietyController.php
+++ src/Controller/SocietyController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Society;
+use App\Form\SocietyType;
 use App\Repository\SocietyRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -18,6 +20,18 @@ class SocietyController extends AbstractController
         $societies = $societyRepository->findBy([],["label" => "ASC"]);
         return $this->render('society/index.html.twig', [
             "societies" => $societies,
+        ]);
+    }
+
+    #[Route('/society/new', name: 'new_society')]
+    public function new(Request $request): Response
+    {
+        $society = new Society();
+
+        $form = $this->createForm(SocietyType::class, $society );
+
+        return $this->render('society/new.html.twig', [
+            'formAddSociety' => $form,
         ]);
     }
 
