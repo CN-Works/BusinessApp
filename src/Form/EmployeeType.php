937dde2f68cdb891b52fd64a88e10dafaa40e875
ChernyyOrel <92865037+ChernyyWorks@users.noreply.github.com>
2023-07-24 02:28:37 +0200

    Style : Create forms

--- src/Form/EmployeeType.php
+++ src/Form/EmployeeType.php
@@ -2,22 +2,59 @@
 
 namespace App\Form;
 
+use App\Entity\Society;
 use App\Entity\Employee;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class EmployeeType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('firstname')
-            ->add('lastname')
-            ->add('dateofbirth')
-            ->add('datehired')
-            ->add('city')
-            ->add('society')
+            ->add('firstname', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('dateofbirth', DateType::class, [
+                "widget" => "single_text",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('datehired', DateType::class, [
+                "widget" => "single_text",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('city', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('society', EntityType::class, [
+                "class" => Society::class,
+                "attr" => [
+                    "class" => "form-select"
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                "attr" => [
+                    "class" => "btn btn-primary"
+                ]
+            ])
         ;
     }
 
--- src/Form/SocietyType.php
+++ src/Form/SocietyType.php
@@ -6,17 +6,46 @@ use App\Entity\Society;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class SocietyType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('label')
-            ->add('creationdate')
-            ->add('adress')
-            ->add('postalcode')
-            ->add('city')
+            ->add('label', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('creationdate', DateType::class, [
+                "widget" => "single_text",
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('adress', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('postalcode', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('city', TextType::class, [
+                "attr" => [
+                    "class" => "form-control"
+                ]
+            ])
+            ->add('submit', SubmitType::class,[
+                "attr" => [
+                    "class" => "btn btn-primary"
+                ]
+            ])
         ;
     }
 
